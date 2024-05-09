ca7abe1626c6ba2fc821ea0c14d19d6384e80fb9
Amaan <amaanhashmi98@gmail.com>
2024-05-09 11:17:46 +0530

    My system 9th April 2024

---
+++ src/Assignments/LeapYear.java
@@ -0,0 +1,17 @@
+package Assignments;
+
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        // Lear Year program - 2024
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.println("Leap year! ");
+        } else {
+            System.out.println("Not a leap year");
+        }
+    }
+}
