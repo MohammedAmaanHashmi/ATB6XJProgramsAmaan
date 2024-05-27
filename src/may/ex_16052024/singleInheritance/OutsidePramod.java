9ed180ada4608df3f6c39173c7fa3c83ba5d29a1
Amaan <amaanhashmi98@gmail.com>
2024-05-27 11:36:08 +0530

    My System OOPS

---
+++ src/may/ex_16052024/singleInheritance/OutsidePramod.java
@@ -0,0 +1,10 @@
+package may.ex_16052024.singleInheritance;
+
+import may.ex_16052024.singleInheritance.pramodHome.Father;
+
+public class OutsidePramod {
+    public static void main(String[] args) {
+        Father fa=new Father();
+        fa.bhk2();
+    }
+}
--- src/may/ex_16052024/singleInheritance/pramodHome/Father.java
+++ src/may/ex_16052024/singleInheritance/pramodHome/Father.java
@@ -1,7 +1,7 @@
 package may.ex_16052024.singleInheritance.pramodHome;
 
 public class Father {
-    void bhk2() {
+    public void bhk2() {
         System.out.println("Father 2BHK House");
     }
 }
