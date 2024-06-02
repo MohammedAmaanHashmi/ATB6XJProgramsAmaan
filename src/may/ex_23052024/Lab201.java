8bde345872087e87312a31d1d131c66698438901
Amaan <amaanhashmi98@gmail.com>
2024-06-02 15:24:14 +0530

    My System

--- src/may/ex_23052024/Lab201.java
+++ src/may/ex_23052024/Lab201.java
@@ -1,23 +1,14 @@
-package may.ex_21052024.staticKeyword;
+package may.ex_23052024;
 
 public class Lab201 {
-    //Static
-    //Data member
-    //Funciton
-    //Class
-
-    //Static loads when class is loaded.
-
     public static void main(String[] args) {
         ATB amit = new ATB();
         amit.id = "1";
         System.out.println(ATB.courseName);
     }
-
 }
-
 
 class ATB {
     static String courseName = "ATB";
-    String id;
+    String phoneNo;
 }
